bea67d7e63f8f36b50fa042fea570b2b40903664
shuhbangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2021-07-05 07:32:14 +0530

    CPP

---
+++ FINAL450/Square_root_of_a_number.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/sqrtx/
+//Square root of a number
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        long int i,res;
+        
+        for( i=0;i*i<=x;i++)
+            res=i*i;
+        
+        if(res<=x)
+            return i-1;
+        
+        return -1;
+    }
+};
