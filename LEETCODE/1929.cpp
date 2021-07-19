f9e08dbc58a6b4ea4ce607b413de28584a3bfe45
shuhbangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2021-07-19 17:45:10 +0530

    Concatenation of Array

--- LEETCODE/1929.cpp
+++ LEETCODE/1929.cpp
@@ -1,5 +1,7 @@
 //1929. Concatenation of Array
 
+
+//method 1
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
@@ -14,5 +16,20 @@ public:
             ans[i+n1]=nums[i];
         }
         return ans;
+    }
+};
+
+
+//method 2
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        int n1=nums.size();
+        
+        for(int i=0;i<n1;i++)
+            nums.push_back(nums[i]);
+        
+        return nums;
     }
 };
