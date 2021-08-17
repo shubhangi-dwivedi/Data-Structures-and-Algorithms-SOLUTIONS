b1597f88cf3d7a24b01755a98bc97b4d9130d0c9
shuhbangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2021-08-17 20:45:06 +0530

    Kth Largest Element in an Array

--- LEETCODE/215.cpp
+++ LEETCODE/215.cpp
@@ -1,10 +1,33 @@
 //215. Kth Largest Element in an Array
 
+//Method 1
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
        sort(nums.begin(),nums.end(),greater<int>());
         
        return nums[k-1];
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> q; //minheap
+        
+        int n1=nums.size();
+        for(int i=0;i<n1;i++){
+            if(i<k)
+                q.push(nums[i]);
+            else{
+                if(nums[i]>q.top()){
+                    q.pop();
+                    q.push(nums[i]);
+                }
+            }
+        }        
+        
+        return q.top();
     }
 };
