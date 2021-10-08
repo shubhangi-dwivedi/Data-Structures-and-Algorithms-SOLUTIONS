1cc4d685a80884d76aea9ebc3b5fbc2e4e6ef2a5
shuhbangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2021-10-08 23:40:43 +0530

    UPDATED

---
+++ LEETCODE-JAVA/FactorialTrailingZeros.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    public int trailingZeroes(int n) {
+        int res=0;
+        
+        for(int i=5;i<=n;i*=5)
+            res=res+n/i;
+        
+        return res;
+    }
+}
