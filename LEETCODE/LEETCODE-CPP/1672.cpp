659592c996ef6ebef01489c4d7a395a439cf8586
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-01-31 23:24:04 +0530

    Richest Customer Wealth

--- LEETCODE/LEETCODE-CPP/1672.cpp
+++ LEETCODE/LEETCODE-CPP/1672.cpp
@@ -1,5 +1,7 @@
 //1672. Richest Customer Wealth
+//https://leetcode.com/problems/richest-customer-wealth/
 
+//Method-1
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
@@ -20,5 +22,25 @@ public:
         res=*max_element(v.begin(),v.end());
         
         return res;
+    }
+};
+
+//Method-2
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& acc) {
+     
+        int ans=INT_MIN;
+        
+        for(int i=0;i<acc.size();i++){
+            int sum=0;
+            
+            for(int j=0;j<acc[0].size();j++)
+                sum+=acc[i][j];
+                
+            ans=max(ans,sum);
+        }
+        
+        return ans;
     }
 };
