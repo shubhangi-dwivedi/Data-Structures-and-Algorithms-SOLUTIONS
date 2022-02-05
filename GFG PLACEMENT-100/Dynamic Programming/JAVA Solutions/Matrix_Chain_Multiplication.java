940449bb078e71dd2937ccc4a1b11fe0dbc84bf5
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-05 14:30:20 +0530

    Matrix Chain Multiplication

---
+++ GFG PLACEMENT-100/Dynamic Programming/JAVA Solutions/Matrix_Chain_Multiplication.java
@@ -0,0 +1,54 @@
+//Matrix Chain Multiplication
+//https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1/#
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.matrixMultiplication(N, arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+
+    //Method-1 (Tabulation) (time-O(n^3))
+    static int matrixMultiplication(int n, int arr[])
+    {
+        // code here
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n-1;i++)
+            dp[i][i+1]=0;
+            
+        for(int gap=2;gap<n;gap++)
+            for(int i=0;i+gap<n;i++){
+                int j=i+gap;
+                dp[i][j]=Integer.MAX_VALUE;
+                
+                for(int k=i+1;k<j;k++)  
+                    dp[i][j]=Math.min(dp[i][j], dp[i][k]+dp[k][j]+arr[i]*arr[k]*arr[j]);
+            }
+            
+            return dp[0][n-1];
+    }
+}
