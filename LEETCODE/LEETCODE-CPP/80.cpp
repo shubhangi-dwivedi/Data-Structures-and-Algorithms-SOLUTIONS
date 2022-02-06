fac4c0b6d88938bc4b729ce865fd3a5d99cbf60f
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-06 20:42:51 +0530

    Remove Duplicates from Sorted Array II

--- LEETCODE/LEETCODE-CPP/80.cpp
+++ LEETCODE/LEETCODE-CPP/80.cpp
@@ -1,5 +1,7 @@
 //80. Remove Duplicates from Sorted Array II
+////https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 
+//1st approach
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -24,5 +26,30 @@ public:
         }
         return x+1;
         
+    }
+};
+
+
+//Approach 2
+class Solution {
+public:
+    int removeDuplicates(vector<int>& a) {
+        int n=a.size();
+        int k=1;
+        int x=0;
+        a[x]=a[0];
+        
+        for(int i=1;i<n;i++){
+            if(a[x]!=a[i]){
+                a[++x]=a[i];
+                k=1;
+            }
+            else if(k<2){
+                k++;
+                a[++x]=a[i];
+            }
+        }
+        
+        return x+1;
     }
 };
