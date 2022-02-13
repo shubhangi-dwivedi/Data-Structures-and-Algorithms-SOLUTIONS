5aedc6b3cd8b3bd2314167d0e24bba4e745e8fe0
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-14 00:15:50 +0530

    5

---
+++ Pepcoding Questions/JAVA- Solutions/Recursion and Backtracking/Introduction to Recursion/Power_Linear.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Power_Linear {
+    
+    public static int pow_calc(int n, int p){
+        if(p==0)
+            return 1;
+
+        if(p==1)
+            return n;
+
+        return n*pow_calc(n, p-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int p=sc.nextInt();
+
+        System.out.println(pow_calc(n,p));
+    }
+}
