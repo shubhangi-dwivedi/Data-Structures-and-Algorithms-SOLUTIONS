cd4b574e91c7384b316d575a22b06cf7bfe753fd
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-14 19:51:07 +0530

    1

--- Pepcoding Questions/JAVA- Solutions/Recursion and Backtracking/Recursion in Arrays/Display_Arrays.java
+++ Pepcoding Questions/JAVA- Solutions/Recursion and Backtracking/Recursion in Arrays/Display_Arrays.java
@@ -1,3 +1,47 @@
+//https://nados.io/question/display-array
+
+/*
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are required to print the elements of array from beginning to end each in a separate line.
+4. For the above purpose complete the body of displayArr function. Don't change the signature.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.                 
+
+Constraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 10
+
+Format
+Input
+A number n
+n1
+n2
+.. n number of elements
+
+Output
+n1
+n2
+.. n elements
+
+Example
+Sample Input
+
+5
+3
+1
+0
+7
+5
+
+Sample Output
+3
+1
+0
+7
+5
+*/
+
 import java.util.*;
 
 public class Display_Arrays {
