e1603d9fe703bae0c6fdef670f6f5c0795addb94
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-14 11:27:24 +0530

    6

---
+++ Pepcoding Questions/JAVA- Solutions/Recursion and Backtracking/Introduction to Recursion/Power_Logarithmic.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Power_Logarithmic {
+
+    public static int calc(int n, int p){
+        if(p==0)
+            return 1;
+
+        int res= calc(n, p/2);
+        int ans= res*res;
+
+        if(p%2==1)
+            ans*=n;
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int p=sc.nextInt();
+
+        int ans=calc(n, p);
+        System.out.println(ans);
+    }
+
+}
