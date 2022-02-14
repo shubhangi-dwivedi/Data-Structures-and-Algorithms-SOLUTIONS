35503bd4da8f99246ac020ac5feef0bd2ed13d43
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-14 19:52:21 +0530

    3

--- Pepcoding Questions/JAVA- Solutions/Recursion and Backtracking/Recursion in Arrays/Max_Of_An_Array.java
+++ Pepcoding Questions/JAVA- Solutions/Recursion and Backtracking/Recursion in Arrays/Max_Of_An_Array.java
@@ -1,3 +1,42 @@
+//https://nados.io/question/max-of-an-array
+
+/*
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are required to find the maximum of input. 
+4. For the purpose complete the body of maxOfArray function. Don't change the signature.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+Constraints
+1 <= n <= 10^4
+0 <= n1, n2, .. n elements <= 10 ^9
+
+Format
+Input
+A number n
+n1
+n2
+.. n number of elements
+
+Output
+A number representing max
+
+Example
+Sample Input
+
+6
+15
+30
+40
+4
+11
+9
+
+Sample Output
+40
+*/
+
 import java.util.*;
 
 public class Max_Of_An_Array {
