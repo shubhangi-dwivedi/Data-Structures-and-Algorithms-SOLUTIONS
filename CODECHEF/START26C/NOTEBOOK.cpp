b0378010e836909c0452d2d3a3f2d923173f1065
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-16 20:39:47 +0530

    1

---
+++ CODECHEF/START26C/NOTEBOOK.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        float n,x;
+        cin>>n;
+
+        x=n*1000;
+        cout<<x/100<<endl;
+    }
+}
