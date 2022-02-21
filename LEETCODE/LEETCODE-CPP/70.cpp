c294dd4aca4b988b849584c44789e7cc9590817e
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-02-21 19:08:16 +0530

    Climbing Stairs

--- LEETCODE/LEETCODE-CPP/70.cpp
+++ LEETCODE/LEETCODE-CPP/70.cpp
@@ -46,3 +46,20 @@ public:
         return dp[n];
     }
 };
+
+
+//Method-3 (dp-tabulation)
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        
+        return dp[n];
+    }
+};
