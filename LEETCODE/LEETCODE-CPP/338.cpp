8cb2693dfed452bce7f9df4bef296fd39d028f4a
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-03-01 20:55:36 +0530

    Counting Bits

--- LEETCODE/LEETCODE-CPP/338.cpp
+++ LEETCODE/LEETCODE-CPP/338.cpp
@@ -1,6 +1,7 @@
 //338. Counting Bits
 //https://leetcode.com/problems/counting-bits/
 
+//Method-1 (Dp-solution)    (time complexity - O(N)     space complexity - O(N))
 class Solution {
 public:
     vector<int> countBits(int n) {
@@ -12,3 +13,5 @@ public:
         return res;
     }
 };
+
+//Method-2 (brute force)    (time complexity - O(nlogn))
