d257956e4b4a5c41cf3450eb0b6151b4768b2952
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-03-10 11:25:59 +0530

    Add Two Numbers

--- LEETCODE/LEETCODE-CPP/2.cpp
+++ LEETCODE/LEETCODE-CPP/2.cpp
@@ -1,4 +1,5 @@
 //2. Add Two Numbers
+//https://leetcode.com/problems/stream-of-characters/
 
 /**
  * Definition for singly-linked list.
@@ -10,6 +11,47 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Method-1
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* res=new ListNode(0);
+        ListNode* curr=res, *p=l1, *q=l2;
+        
+        
+        int carry=0;
+        while(p!=NULL || q!=NULL){
+            
+            int x=0;
+            if(p!=NULL)
+                x=p->val;
+            
+            int y=0;
+            if(q!=NULL)
+                y=q->val;
+            
+            int sum=carry+x+y;
+            curr->next=new ListNode(sum%10);
+            curr=curr->next;
+            
+            carry=sum/10;
+            
+            if(p!=NULL)
+                p=p->next;
+            if(q!=NULL)
+                q=q->next;
+        }
+        
+        if(carry>0)
+            curr->next=new ListNode(carry);
+    
+        return res->next;
+    }
+};
+
+
+//Method-2 (clear code of Method-1)
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -45,3 +87,4 @@ public:
         
     }
 };
+
