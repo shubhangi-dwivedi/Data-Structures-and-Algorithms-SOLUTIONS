8d36a947b3ae4aaa5dcc3dbf673c569fde047af2
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-03-15 19:53:48 +0530

    Minimum Remove to Make Valid Parentheses

---
+++ LEETCODE/LEETCODE-CPP/1249.cpp
@@ -0,0 +1,36 @@
+//1249. Minimum Remove to Make Valid Parentheses
+//https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> stk;
+        int l=s.length();
+        
+        for(int i=0;i<l;i++){            
+            if(s[i]=='('){
+                stk.push(i);
+            }
+            else if(s[i]==')'){
+                if(!stk.empty())
+                    stk.pop();
+                else
+                    s[i]='_';
+            }   
+        }
+        
+        cout<<s;
+        
+        while(!stk.empty()){
+            s[stk.top()]='_';
+            stk.pop();
+        }
+        
+        string res;
+        for(char c : s)
+            if(c!='_')
+                res+=c;
+        
+        return res;
+    }
+};
