3641a326f34700a93a5bd304dcf6013aa1aa49ed
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-03-27 02:05:24 +0530

    The Rating Dilemma

---
+++ CODECHEF/MARCH222C/RATING.cpp
@@ -0,0 +1,20 @@
+//The Rating Dilemma
+//https://www.codechef.com/MARCH222C/problems/RATING
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+
+    cin>>t;
+    while(t--){
+        int s;
+        cin>>s;
+
+        int y=-1-s;
+        cout<<y<<endl;
+
+    }
+}
