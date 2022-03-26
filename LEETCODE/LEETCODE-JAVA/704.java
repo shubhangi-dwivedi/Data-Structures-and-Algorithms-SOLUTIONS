29c9507be781971459259c7c329a719aab3d1f29
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-03-26 19:02:37 +0530

    Binary Search

---
+++ LEETCODE/LEETCODE-JAVA/704.java
@@ -0,0 +1,22 @@
+//704. Binary Search
+//https://leetcode.com/problems/binary-search/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int s=0, e=n-1;
+        
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                e=mid-1;
+            else
+                s=mid+1;
+        }
+        
+        return -1;
+    }
+}
