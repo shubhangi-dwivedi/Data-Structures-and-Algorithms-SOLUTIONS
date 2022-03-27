c018b1e712ac9533e0742fd2f7d49520f8178c76
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-03-27 09:51:36 +0530

    Missing Numbers

---
+++ CODECHEF/MARCH222C/MISS_NUM.cpp
@@ -0,0 +1,34 @@
+//Missing Numbers 
+//https://www.codechef.com/MARCH222C/problems/MISS_NUM
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+int main(){
+    int t;
+
+    cin>>t;
+    while(t--){
+        int a[4];
+        for(int i=0;i<4;i++)
+            cin>>a[i];
+
+        sort(a,a+4);
+
+        if(a[0]>=0)
+            cout<<-1<<" "<<-1<<endl;
+
+
+        int x=(a[2]+a[0])/2;
+        int y=a[2]-x;
+
+        if((x+y==a[2]) && (x-y==a[0]) && (x*y==a[3]) && (x/y==a[1])){
+            cout<<x<<" "<<y<<endl;
+        }
+        else
+            cout<<-1<<" "<<-1<<endl;
+    }
+}
+*/
