3f9f9d628207b9fa9e14b439db04b8e2cdb826b0
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-04-02 21:14:19 +0530

    Valid Palindrome II

---
+++ LEETCODE/LEETCODE-CPP/680.cpp
@@ -0,0 +1,32 @@
+//680. Valid Palindrome II
+//https://leetcode.com/problems/valid-palindrome-ii/
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l=s.length(), count=0;
+        
+        return check(s,count,0,l-1);
+    }
+    
+    bool check(string s,int count, int i,int j){
+        while(i<=j){
+            if(s[i]!=s[j]){
+                
+                if(count==0){
+                    count++;
+                    return check(s,count,i+1,j) || check(s,count,i,j-1);
+                }
+                else
+                    return false;
+            }
+            else
+                i++,j--;
+        }
+        
+        if(count>1)
+            return false;
+        
+        return true;
+    }
+};
