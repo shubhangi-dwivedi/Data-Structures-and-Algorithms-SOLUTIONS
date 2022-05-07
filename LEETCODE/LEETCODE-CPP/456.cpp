950cd70b1b85592d5dd15f7a117ee1f81d37c3f9
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-05-07 21:49:51 +0530

    132 Pattern

---
+++ LEETCODE/LEETCODE-CPP/456.cpp
@@ -0,0 +1,25 @@
+//456. 132 Pattern
+//https://leetcode.com/problems/132-pattern/
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+       int s3 = INT_MIN;
+       int n = nums.size();
+        stack<int>s;
+        for(int i = n-1;i>=0;i--)
+        {
+            if(nums[i]<s3)
+                return true;
+            else
+                while(!s.empty() && nums[i]>s.top())
+                {
+                    s3 = s.top();
+                    s.pop();
+                }
+            
+            s.push(nums[i]);
+        }
+        return false;
+    }
+};
