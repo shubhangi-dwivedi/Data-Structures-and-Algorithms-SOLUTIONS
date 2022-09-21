34328b3a576e717ec129d15d0aefdba6599140c3
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-09-21 13:11:25 +0530

    1757. Recyclable and Low Fat Products

--- LEETCODE/LEETCODE-SQL/1757.sql
+++ LEETCODE/LEETCODE-SQL/1757.sql
@@ -1,8 +1,22 @@
-//1757. Recyclable and Low Fat Products
-//https://leetcode.com/problems/recyclable-and-low-fat-products/
+/* 1757. Recyclable and Low Fat Products */
+/* https://leetcode.com/problems/recyclable-and-low-fat-products/ */
 
 /* Write your PL/SQL query statement below */
+
+/*Method-1 (using =) */
 select product_id 
 from Products
 where
 low_fats='Y' and recyclable='Y';
+
+/* Method-2 (using in) */
+select product_id
+from Products
+where 
+low_fats in ('Y') and recyclable in ('Y');
+
+/* Method-3 (using like) */
+select product_id 
+from Products 
+where 
+low_fats like 'Y%' and recyclable like 'Y%';
