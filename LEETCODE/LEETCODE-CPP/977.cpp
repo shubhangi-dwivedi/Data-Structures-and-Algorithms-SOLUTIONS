fb7844a29fe914e48b102f62db8147d60fda728d
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-09-22 19:53:17 +0530

    977. Squares of a Sorted Array

--- LEETCODE/LEETCODE-CPP/977.cpp
+++ LEETCODE/LEETCODE-CPP/977.cpp
@@ -1,5 +1,7 @@
 //977. Squares of a Sorted Array
+//https://leetcode.com/problems/squares-of-a-sorted-array/
 
+//Method-1 (brute force)
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) 
@@ -10,5 +12,26 @@ public:
         sort(nums.begin() , nums.end());
         
         return nums;
+    }
+};
+
+//Method-2 (two pointers)
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int start=0, n=nums.size(), end=n-1;
+        vector<int> res(n,0);
+        
+        for(int i=n-1;i>=0;i--){
+            if(abs(nums[start])>nums[end]){
+                res[i]=pow(nums[start],2);
+                start++;
+            }
+            else{
+                res[i]=pow(nums[end],2);
+                end--;
+            }
+        }
+        return res;
     }
 };
