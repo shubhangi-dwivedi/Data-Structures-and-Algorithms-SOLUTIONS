16f47b26cfe9ec4b8ab246b84b60d3515bcc1600
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-09-23 21:54:39 +0530

    1667. Fix Names in a Table

--- LEETCODE/LEETCODE-SQL/1667.sql
+++ LEETCODE/LEETCODE-SQL/1667.sql
@@ -2,5 +2,10 @@
 /* https://leetcode.com/problems/fix-names-in-a-table/ */
 
 /* Write your PL/SQL query statement below */
+
+/*Method-1*/
 SELECT user_id, CONCAT(UPPER(SUBSTR(name, 1, 1)), LOWER(SUBSTR(name, 2))) AS name
 FROM Users ORDER BY user_id;
+
+/*Method-2*/
+select user_id, initcap(name) as name from users order by user_id;
