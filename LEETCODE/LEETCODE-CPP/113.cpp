75f791e219a1e8924735de685b942cfb7083159e
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-09-24 21:44:07 +0530

    113. Path Sum II

---
+++ LEETCODE/LEETCODE-CPP/113.cpp
@@ -0,0 +1,44 @@
+//113. Path Sum II
+//https://leetcode.com/problems/path-sum-ii/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int> temp;
+        
+        calc(root,targetSum,temp);
+        return res;
+    }
+    
+    void calc(TreeNode* root, int ts, vector<int> temp){
+        if(root==NULL)
+            return;
+        
+        temp.push_back(root->val);
+        ts-=root->val;
+        
+        if(root->left==NULL && root->right==NULL){
+            if(ts==0)
+                res.push_back(temp);
+        }
+        else{
+            calc(root->left, ts, temp);
+            calc(root->right, ts, temp);
+        }
+         
+        temp.pop_back();
+    }
+};
