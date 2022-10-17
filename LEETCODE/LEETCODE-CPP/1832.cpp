5a79f2654eadd283394e10240568a67c132e6282
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-10-17 20:55:04 +0530

    1832. Check if the Sentence Is Pangram

--- LEETCODE/LEETCODE-CPP/1832.cpp
+++ LEETCODE/LEETCODE-CPP/1832.cpp
@@ -1,5 +1,7 @@
 //1832. Check if the Sentence Is Pangram
+//https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
 
+//Method-1 (using map)
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
@@ -12,5 +14,40 @@ public:
             return true;
         
         return false;
+    }
+};
+
+
+//Method-2 (using set)
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        unordered_set<char>s2;
+        for(int i=0;i<s.length();i++)
+            s2.insert(s[i]);
+
+        if(s2.size()==26)
+            return true;
+
+        return false;
+    }
+};
+
+
+//Method-3 (using array)
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        vector<int> v(26,0);
+
+        for(int i=0;i<s.size();i++)
+            if(v[s[i]-'a']==0)
+                v[s[i]-'a']++;
+        
+        for(int i=0;i<v.size();i++)
+            if(v[i]!=1)
+                return false;
+
+        return true;
     }
 };
