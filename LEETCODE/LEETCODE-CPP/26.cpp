464d2b2e01e65c5cb65a82e94165e43c7671ed46
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2022-11-11 14:03:01 +0530

    26. Remove Duplicates from Sorted Array

--- LEETCODE/LEETCODE-CPP/26.cpp
+++ LEETCODE/LEETCODE-CPP/26.cpp
@@ -6,12 +6,7 @@ public:
     int removeDuplicates(vector<int>& nums)
     {
         int n,x=0;
-        
         n=nums.size();
-        
-        if(n==0)
-            return 0;
-        
         
         for(int i=1;i<n;i++)
             if(nums[x]!=nums[i])
