d6c441d7423ab7e8fb03d11aa659813f3c3e2a3f
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2023-06-24 01:15:08 +0530

    189. Rotate Array

--- LEETCODE/LEETCODE-CPP/189.cpp
+++ LEETCODE/LEETCODE-CPP/189.cpp
@@ -1,6 +1,7 @@
 //189. Rotate Array
 //https://leetcode.com/problems/rotate-array/
 
+//Method - 1
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -22,5 +23,24 @@ public:
         
         reverse(nums.begin()+k,nums.end());
         
+    }
+};
+
+//Method - 2 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size(), temp[n];
+        int x=k%n;
+
+        for(int i=0;i<x;i++)
+            temp[i]=nums[n-x+i];
+
+        for(int i=x;i<n;i++)
+            temp[i]=nums[i-x];
+
+        for(int i=0;i<n;i++)
+            nums[i]=temp[i];
+
     }
 };
