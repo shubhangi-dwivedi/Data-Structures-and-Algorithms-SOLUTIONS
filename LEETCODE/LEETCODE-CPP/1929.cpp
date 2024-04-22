4978cab1f2ca0ff41242119568cd43ae88f0f20b
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2024-04-22 23:06:53 +0530

    1929. Concatenation of Array

--- LEETCODE/LEETCODE-CPP/1929.cpp
+++ LEETCODE/LEETCODE-CPP/1929.cpp
@@ -1,35 +1,37 @@
-//1929. Concatenation of Array
+// 1929. Concatenation of Array
+// https://leetcode.com/problems/concatenation-of-array/
 
-
-//method 1
-class Solution {
+// method 1
+class Solution
+{
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        
-        int n1=nums.size(),n2=2*n1;
-        
-        vector<int> ans(n2,0);
-        
-        for(int i=0;i<n1;i++)
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n * 2, 0);
+
+        for (int i = 0; i < n; i++)
         {
-            ans[i]=nums[i];
-            ans[i+n1]=nums[i];
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
         }
+
         return ans;
     }
 };
 
-
-//method 2
-class Solution {
+// method 2
+class Solution
+{
 public:
-    vector<int> getConcatenation(vector<int>& nums) {
-        
-        int n1=nums.size();
-        
-        for(int i=0;i<n1;i++)
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+
+        int n1 = nums.size();
+
+        for (int i = 0; i < n1; i++)
             nums.push_back(nums[i]);
-        
+
         return nums;
     }
 };
