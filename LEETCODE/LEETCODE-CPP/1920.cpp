eec1006bc962e1ddb1cf379df561d89f1c614ae5
shubhangi-dwivedi <shubhangi.dwivedi04@gmail.com>
2024-04-23 00:14:54 +0530

    1920. Build Array from Permutation

--- LEETCODE/LEETCODE-CPP/1920.cpp
+++ LEETCODE/LEETCODE-CPP/1920.cpp
@@ -1,13 +1,16 @@
-//1920. Build Array from Permutation
+// 1920. Build Array from Permutation
+// https://leetcode.com/problems/build-array-from-permutation/description/
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> buildArray(vector<int>& nums) {
+    vector<int> buildArray(vector<int> &nums)
+    {
         vector<int> res;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
             res.push_back(nums[nums[i]]);
-        
+
         return res;
     }
 };
