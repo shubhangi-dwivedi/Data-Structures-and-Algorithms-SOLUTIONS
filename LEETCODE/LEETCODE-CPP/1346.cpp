e7b4e6e360505bf140d359dddb0b89ba3e04a90f
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-07-25 22:20:10 +0530

    1346. Check If N and Its Double

--- LEETCODE/LEETCODE-CPP/1346.cpp
+++ LEETCODE/LEETCODE-CPP/1346.cpp
@@ -1,5 +1,7 @@
 //1346. Check If N and Its Double Exist
+//https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
 
+//Method-1 brute force
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
@@ -14,6 +16,28 @@ public:
                 }
             }
         }
+        return false;
+    }
+};
+
+
+//Methos-2 using maps
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        map<int,int> mp;
+        int n=arr.size();
+
+        for(int i=0;i<n;i++)
+            mp[arr[i]]++;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]==0 && mp[arr[i]]>=2)
+                return true;
+            if(arr[i]!=0 && mp[arr[i]*2]>=1)
+                return true;
+        }
+
         return false;
     }
 };
