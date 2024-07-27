dfa00f7514bbc9fb1ab9170668b9c55173164465
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-07-27 14:29:22 +0530

    2418. Sort the People

--- LEETCODE/LEETCODE-CPP/2418.cpp
+++ LEETCODE/LEETCODE-CPP/2418.cpp
@@ -1,6 +1,27 @@
 //2418. Sort the People
 //https://leetcode.com/problems/sort-the-people/description/
 
+//method-1 using sorting
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n=heights.size();
+        vector<pair<int,string>> temp;
+
+        for(int i=0;i<n;i++)
+            temp.push_back({heights[i],names[i]});
+
+        sort(temp.rbegin(), temp.rend());
+
+        for(int i=0;i<n;i++)
+            names[i]=temp[i].second;
+
+        return names;
+    }
+};
+
+
+//Methos-2 using priority queue
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
