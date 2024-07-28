5b93719b38deabca3f9b35763bb131699735df2b
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-07-28 13:27:27 +0530

    338. Counting Bits

--- LEETCODE/LEETCODE-CPP/338.cpp
+++ LEETCODE/LEETCODE-CPP/338.cpp
@@ -15,3 +15,25 @@ public:
 };
 
 //Method-2 (brute force)    (time complexity - O(nlogn))
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+
+        for(int i=0;i<=n;i++)
+            ans.push_back(helper(i));
+        
+        return ans;
+    }
+
+    int helper(int x){
+        int count=0;
+
+        while(x!=0){
+            x=x&(x-1);
+            count++;
+        }
+        
+        return count;
+    }
+};
