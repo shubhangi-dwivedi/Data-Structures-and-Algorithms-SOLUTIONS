b3ba888430a331840227471941363c254042af3a
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-08-01 23:29:54 +0530

    69. Sqrt(x)

--- LEETCODE/LEETCODE-CPP/69.cpp
+++ LEETCODE/LEETCODE-CPP/69.cpp
@@ -1,5 +1,7 @@
 //69. Sqrt(x)
+//https://leetcode.com/problems/sqrtx/
 
+//Method-1 (brute force)
 class Solution {
 public:
     int mySqrt(int x) {
@@ -13,5 +15,29 @@ public:
             return i-1;
         
         return -1;
+    }
+};
+
+
+//Method-2 (binary search)
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==1)
+            return 1;
+        long low=0, high=x/2;
+
+        while(low<=high){
+            long mid=low+(high-low)/2;
+
+            if(mid*mid==x)
+                return mid;
+            else if(mid*mid>x)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+
+        return high;
     }
 };
