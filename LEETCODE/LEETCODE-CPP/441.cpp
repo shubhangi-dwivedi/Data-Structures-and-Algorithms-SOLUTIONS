fcf308def73869ec8e57386055cc4c3862265f97
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-08-02 00:46:27 +0530

    441. Arranging Coins

--- LEETCODE/LEETCODE-CPP/441.cpp
+++ LEETCODE/LEETCODE-CPP/441.cpp
@@ -21,3 +21,25 @@ public:
         return count-1;
     }
 };
+
+//Methos-2 (binary search)
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int low=1, high=n;
+
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            long long sum=(mid*(mid+1))/2;
+
+            if(sum==n)
+                return mid;
+            else if(sum>n)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+
+        return high;
+    }
+};
