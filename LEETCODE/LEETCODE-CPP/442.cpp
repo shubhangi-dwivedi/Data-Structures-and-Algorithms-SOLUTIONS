f71d2bac8e95958724fb9eb9da339671565236cc
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-06 01:47:55 +0530

    442. Find All Duplicates in an Array

--- LEETCODE/LEETCODE-CPP/442.cpp
+++ LEETCODE/LEETCODE-CPP/442.cpp
@@ -1,7 +1,8 @@
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
 //442. Find All Duplicates in an Array
 
 
-//Not O(1) space
+//Method-1 Not O(1) space
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
@@ -15,6 +16,42 @@ public:
         for(auto i:m)
             if(i.second==2)
                 res.push_back(i.first);
+        
+        return res;
+    }
+};
+
+//Method-2 using sorting
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> res;
+        sort(nums.begin(), nums.end());
+
+        for(int i=1;i<n;i++)
+            if(nums[i]==nums[i-1])
+                res.push_back(nums[i]);
+        
+        return res;
+    }
+};
+
+//Method-3 marking the visited index
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> res;
+
+        for(int i=0;i<n;i++){
+            int k=abs(nums[i])-1;
+
+            if(nums[k]<0)
+                res.push_back(abs(nums[i]));
+            else
+                nums[k]*=-1;
+        }
         
         return res;
     }
