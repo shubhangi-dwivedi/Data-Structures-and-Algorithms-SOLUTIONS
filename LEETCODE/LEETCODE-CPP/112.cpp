4f39fe2d28478ed558a63fe9cca10774dfb8267f
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-08 03:11:12 +0530

    112. Path Sum

--- LEETCODE/LEETCODE-CPP/112.cpp
+++ LEETCODE/LEETCODE-CPP/112.cpp
@@ -34,3 +34,36 @@ public:
         return (calc(root->left, ts) || calc(root->right, ts));
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum=0;
+
+        if(root==NULL)
+            return false;
+
+        return helper(root, targetSum, sum);
+    }
+
+    bool helper(TreeNode* root, int targetSum, int sum){
+        if(root==NULL)
+            return false;
+            
+        if(root->left==NULL && root->right==NULL)
+            return targetSum==sum+root->val;
+
+        return helper(root->left, targetSum, sum+root->val) || helper(root->right, targetSum, sum+root->val);
+    }
+};
