cec9405264170a43d844f2f2a19bc18ed84897bc
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-09 01:07:26 +0530

    198. House Robber

--- LEETCODE/LEETCODE-CPP/198.cpp
+++ LEETCODE/LEETCODE-CPP/198.cpp
@@ -1,3 +1,7 @@
+//198. House Robber
+//https://leetcode.com/problems/house-robber/description/
+
+//Method- 1 Brute Force
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -19,9 +23,54 @@ public:
             }
                 
             
-        }
-       
-        
+        }    
         return max(sum1,sum2);
+    }
+};
+
+
+//Method -3 Recursion
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size()-1;
+
+        return helper(nums, n);
+    }
+
+    int helper(vector<int>& nums, int n){
+        if(n<0)
+            return 0;
+
+        int sum1= helper(nums, n-2)+nums[n];
+        int sum2= helper(nums, n-1);
+
+        return max(sum1,sum2);
+    }
+};
+
+
+//Method -3 DP
+class Solution {
+public:
+    int dp[105];
+    int rob(vector<int>& nums) {
+        int n=nums.size()-1;
+        memset(dp, -1, sizeof(dp));
+
+        return helper(nums, n);
+    }
+
+    int helper(vector<int>& nums, int n){
+        if(n<0)
+            return 0;
+        
+        if(dp[n] != -1)
+            return dp[n];
+
+        int sum1= helper(nums, n-2)+nums[n];
+        int sum2= helper(nums, n-1);
+
+        return dp[n]=max(sum1,sum2);
     }
 };
