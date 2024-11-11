7836cc8babc7c2dd73dad85628c33f5862524735
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-12 02:04:18 +0530

    169. Majority Element

--- LEETCODE/LEETCODE-CPP/169.cpp
+++ LEETCODE/LEETCODE-CPP/169.cpp
@@ -61,3 +61,35 @@ public:
 };
 
 //Method-4 Moore's voting algo
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size(), count=1, res=nums[0];
+
+        for(int i=1;i<n;i++){
+            if(res==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+
+            if(count==0){
+                res=nums[i];
+                count=1;
+            }
+        }
+
+        count=0;
+        for(int i=0;i<n;i++){
+            if(res==nums[i]){
+                count++;
+            }
+
+            if(count>=n/2){
+                return res;
+            }
+        }
+       return -1;
+    }
+};
