fb18828cda235d50abab145c7b1ca627f94619e0
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-13 22:02:41 +0530

    128. Longest Consecutive Sequence

--- LEETCODE/LEETCODE-CPP/128.cpp
+++ LEETCODE/LEETCODE-CPP/128.cpp
@@ -25,7 +25,7 @@ public:
                 currentLength++;
             }
 
-            longest = max(longest, currentLength);
+            longest = max(longest,currentLength);
         }
 
         return longest;
@@ -33,4 +33,77 @@ public:
 };
 
 
-//
+//Method-2 (Sorting)
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size(),len1 = 1;
+
+        if(n==0){
+            return 0;
+        }
+        
+        sort(nums.begin(),nums.end());
+        
+        int lastsmallest= nums[0], len2=1;
+
+        for(int i=1; i<n; i++){
+            if(nums[i]==lastsmallest){
+                continue;
+            }
+            else if(nums[i]==lastsmallest+1){
+                len2++;
+            }
+            else{
+                len2=1;
+                
+            }
+            lastsmallest=nums[i];
+            len1=max(len1,len2);
+        }
+
+        return len1;
+    }
+};
+
+
+//Method-3 optimised using sets
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size(),len=0;
+
+        if(n==0){
+            return 0;
+        }
+
+        unordered_set<int> s;
+
+        for(int i=0; i<n; i++){
+            s.insert(nums[i]);
+        }
+
+
+        for(auto it:s){
+            if(s.find(it-1)==s.end()){
+                int x=it;
+                int count=1;
+                while(s.find(x+1)!=s.end()){
+                    count++;
+                    x+=1;
+                }
+                len=max(len,count);
+            }
+        }
+
+        return len;
+    }
+};
