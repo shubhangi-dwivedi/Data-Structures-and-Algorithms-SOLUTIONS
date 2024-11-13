adab04199cb236031de7ae27998ec0d7bf450961
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-14 02:15:10 +0530

    784. Letter Case Permutation

--- LEETCODE/LEETCODE-CPP/784.cpp
+++ LEETCODE/LEETCODE-CPP/784.cpp
@@ -1,5 +1,7 @@
 //784. Letter Case Permutation
+//https://leetcode.com/problems/letter-case-permutation/description/
 
+//Method-1 Brute force
 class Solution {
 public:
     vector<string> letterCasePermutation(string s) {
@@ -18,6 +20,43 @@ public:
                 }
             }
         return res;
+        
+    }
+};
+
+//Method-2 Recursion & backtracking
+class Solution {
+public:
+    vector<string> letterCasePermutation(string s) {
+        int n=s.length();
+        vector<string> ans;
+        set<string> temp;
+
+        helper(s,temp,0,n);
+        ans={temp.begin(),temp.end()};
+
+        return ans;
+    }
+
+    void helper(string &s, set<string>&temp,int idx, int n){
+        if(idx==n){
+            temp.insert(s);
+            return;
+        }
+
+        helper(s,temp,idx+1,n);
+
+        if(islower(s[idx])){
+            s[idx]=toupper(s[idx]);
+            helper(s,temp,idx+1,n);
+            s[idx]=tolower(s[idx]);
+        }
+        else if(isupper(s[idx])){
+            s[idx]=tolower(s[idx]);
+            helper(s,temp,idx+1,n);
+            s[idx]=toupper(s[idx]);
+        }
+            
         
     }
 };
