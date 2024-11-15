3b207cb319b6ce0b9a723dfc26f5be936b4e325c
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-15 10:46:44 +0530

    40. Combination Sum II

--- LEETCODE/LEETCODE-CPP/40.cpp
+++ LEETCODE/LEETCODE-CPP/40.cpp
@@ -38,3 +38,40 @@ public:
 
     }
 };
+
+
+//Method-2 recursion and backtracking
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        sort(candidates.begin(), candidates.end());
+
+        helper(candidates,ans,temp,target,0);
+
+        return ans;
+    }
+
+    void helper(vector<int>& candidates, vector<vector<int>> &ans, vector<int> temp, int target, int idx){
+
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=idx; i<candidates.size() ;i++){
+            if(i>idx && candidates[i]==candidates[i-1]){
+                continue;
+            }
+
+            if(candidates[i]>target)
+                break;
+
+            temp.push_back(candidates[i]);
+            helper(candidates,ans,temp,target-candidates[i],i+1);
+            temp.pop_back();
+        }
+    }
+};
