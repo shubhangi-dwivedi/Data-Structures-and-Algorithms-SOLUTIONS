2759b536178ed5be69d2545d357280dd067ab3ce
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-16 22:02:20 +0530

    49. Group Anagrams

--- LEETCODE/LEETCODE-CPP/49.cpp
+++ LEETCODE/LEETCODE-CPP/49.cpp
@@ -27,3 +27,38 @@ public:
         return ans;
     }
 };
+
+
+//Method-1 using hashmap and without sorting
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n=strs.size();
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+        if(n<0){
+            return ans;
+        }
+
+        for(auto s:strs){
+            vector<int> arr(26,0);
+            for(auto x:s){
+                arr[x-'a']++;
+            }
+            
+            string temp="";
+            for(int i=0;i<26;i++){
+                temp+=to_string(arr[i]);
+                temp=temp+'0';
+            }
+
+            mp[temp].push_back(s);
+        }
+
+        for(auto i=mp.begin();i!=mp.end();i++){
+            ans.push_back(i->second);
+        }
+
+        return ans;
+    }
+};
