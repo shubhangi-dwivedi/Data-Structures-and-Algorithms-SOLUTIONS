e0d2f520a29ed0454033c86aeaac86b49950a0d1
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-16 20:58:02 +0530

    49. Group Anagrams

---
+++ LEETCODE/LEETCODE-CPP/49.cpp
@@ -0,0 +1,26 @@
+//49. Group Anagrams
+//https://leetcode.com/problems/group-anagrams/description/
+
+
+//Method-1 using hashmap and sorting
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n=strs.size();
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+
+        for(int i=0;i<n;i++){
+            string temp=strs[i];
+
+            sort(strs[i].begin(),strs[i].end());
+            mp[strs[i]].push_back(temp);
+        }
+
+        for(auto i=mp.begin();i!=mp.end();i++){
+            ans.push_back(i->second);
+        }
+
+        return ans;
+    }
+};
