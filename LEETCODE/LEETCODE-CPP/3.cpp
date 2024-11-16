460b6273f15bd451a0b78a7997049916448d995e
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-17 01:36:49 +0530

    3. Longest Substring Without Repeating Characters

--- LEETCODE/LEETCODE-CPP/3.cpp
+++ LEETCODE/LEETCODE-CPP/3.cpp
@@ -1,5 +1,7 @@
 //3. Longest Substring Without Repeating Characters
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+//Method-1
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -15,5 +17,34 @@ public:
             maxLen=max(maxLen,i-start);
         }
         return maxLen;
+    }
+};
+
+
+//Method-2 using sliding window
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.length(), ans=INT_MIN;
+        set<char>st;
+
+        if(n==0)
+            return 0;
+        if(n==1){
+            return 1;
+        }
+
+        int j=0;
+        for(int i=0;i<n;i++){
+
+            while(st.find(s[i])!=st.end()){
+                st.erase(s[j]);
+                j++;
+            }
+
+            st.insert(s[i]);
+            ans=max(ans,j-i+1);
+        }
+        return ans;
     }
 };
