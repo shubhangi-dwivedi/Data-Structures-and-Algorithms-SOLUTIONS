88c068351bd55076c574453b07c2dbcc1e44de8f
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-18 02:09:29 +0530

    841. Keys and Rooms

--- LEETCODE/LEETCODE-CPP/841.cpp
+++ LEETCODE/LEETCODE-CPP/841.cpp
@@ -1,5 +1,7 @@
 //841. Keys and Rooms
+//https://leetcode.com/problems/keys-and-rooms/
 
+//Method-1 using BFS
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
@@ -27,5 +29,37 @@ public:
                 return false;
         
         return true;
+    }
+};
+
+
+//Method-2 using DFS
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size(),count=0;
+        vector<bool>vis(n,false);
+
+        helper(rooms,0,vis);
+
+        for(int i=0;i<n;i++){
+            if(vis[i]==false){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void helper(vector<vector<int>>& rooms,int idx,vector<bool> &vis){
+        if(vis[idx]==true){
+            return;
+        }
+        vis[idx]=true;
+        for(auto x: rooms[idx]){
+            if(vis[x]==false){
+                helper(rooms,x,vis);
+            }
+        }
     }
 };
