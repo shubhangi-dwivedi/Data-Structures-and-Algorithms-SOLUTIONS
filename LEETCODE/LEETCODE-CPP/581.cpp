31f4a0a4e2ff44c74ca36a32c1f66859800a2aa0
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-18 01:46:04 +0530

    581. Shortest Unsorted Continuous Subarray

--- LEETCODE/LEETCODE-CPP/581.cpp
+++ LEETCODE/LEETCODE-CPP/581.cpp
@@ -1,6 +1,7 @@
 //581. Shortest Unsorted Continuous Subarray
 //https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
 
+//Method-1 brute force
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
@@ -21,5 +22,52 @@ public:
         if(ans < 0) 
             return 0;
         return ans+1;
+    }
+};
+
+
+//Method-2 brute force
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n=nums.size(),left=-1,right=-1;
+        vector<int>temp=nums;
+        sort(temp.begin(),temp.end());
+
+        for(int i=0;i<n;i++){
+            if(temp[i]!=nums[i]){
+                if(left==-1){
+                    left=i;
+                }
+                right=i;
+            }
+        }
+
+        return left==-1?0:(right-left+1);
+    }
+};
+
+
+//Method-3 optimized
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n=nums.size(),left=-1,right=-1,leastEle=INT_MAX,maxEle=INT_MIN;
+        
+        for(int i=n-1;i>=0;i--){
+            leastEle=min(leastEle,nums[i]);
+            if(leastEle!=nums[i]){
+                left=i;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            maxEle=max(maxEle,nums[i]);
+            if(maxEle!=nums[i]){
+                right=i;
+            }    
+        }
+
+        return left==right?0:(right-left+1);
     }
 };
