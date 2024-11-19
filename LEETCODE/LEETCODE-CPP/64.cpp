c1c08ace0b31e0eb548ef471daf76056f6f77a88
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-19 16:29:57 +0530

    64. Minimum Path Sum

--- LEETCODE/LEETCODE-CPP/64.cpp
+++ LEETCODE/LEETCODE-CPP/64.cpp
@@ -1,8 +1,66 @@
 //64. Minimum Path Sum
 //https://leetcode.com/problems/minimum-path-sum/
 
+//Method-1 Recursion
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(), m=grid[0].size();
 
-//Method-1 (dp-tabulation)
+        return helper(grid,n,m,0,0);
+    }
+
+    int helper(vector<vector<int>> &grid, int n, int m, int i, int j){
+        if(i>=n || j>=m){
+            return INT_MAX;
+        }
+
+        if(i==n-1 &&j==m-1){
+            return grid[i][j];
+        }
+
+        int r=0,d=0;
+        d=helper(grid,n,m,i+1,j);
+        r=helper(grid,n,m,i,j+1);
+
+        return grid[i][j]+ min(r,d);
+    }
+};
+
+
+//Method-2 (dp-memoization)
+class Solution {
+public:
+    int dp[201][201];
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(), m=grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        
+        return helper(grid,n,m,0,0);
+    }
+
+    int helper(vector<vector<int>> &grid, int n, int m, int i, int j){
+        if(i>=n || j>=m){
+            return INT_MAX;
+        }
+
+        if(i==n-1 &&j==m-1){
+            return dp[i][j]=grid[i][j];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int r=0,d=0;
+        d=helper(grid,n,m,i+1,j);
+        r=helper(grid,n,m,i,j+1);
+
+        return dp[i][j]=grid[i][j]+ min(r,d);
+    }
+};
+
+//Method-3 (dp-tabulation)
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
