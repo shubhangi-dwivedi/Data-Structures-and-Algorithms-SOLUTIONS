3bcbc7f112b10b07136f7fd3f4f05b36d94ce219
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-11-19 14:36:30 +0530

    63. Unique Paths II

--- LEETCODE/LEETCODE-CPP/63.cpp
+++ LEETCODE/LEETCODE-CPP/63.cpp
@@ -1,8 +1,68 @@
 //63. Unique Paths II
 //https://leetcode.com/problems/unique-paths-ii/
 
+//Method-1 Recursion
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n=obstacleGrid.size(),m=obstacleGrid[0].size();
+        int paths=0;
 
-//Method-1 (dp-tabulation) (using same matrix)
+        helper(obstacleGrid,n,m,0,0,paths);
+        return paths;
+    }
+
+    void helper(vector<vector<int>>& obstacleGrid, int n, int m, int i ,int j, int &paths){
+        if (i>=n || j>=m || obstacleGrid[i][j] == 1) {
+            return;
+        }
+
+        if(i==n-1 && j==m-1 && obstacleGrid[i][j]!=1){
+            paths++;
+            return;
+        }
+
+        helper(obstacleGrid,n,m,i+1,j,paths);
+        helper(obstacleGrid,n,m,i,j+1,paths);
+        
+    }
+};
+
+
+//Method-2 using dp-memoization
+class Solution {
+public:
+    int dp[101][101];
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n=obstacleGrid.size(),m=obstacleGrid[0].size();
+        memset(dp,-1,sizeof(dp));
+
+        return helper(obstacleGrid,n,m,0,0);
+    }
+
+    int helper(vector<vector<int>>& obstacleGrid, int n, int m, int i ,int j){
+        if (i>=n || j>=m || obstacleGrid[i][j] == 1) {
+            return 0;
+        }
+
+        if(i==n-1 && j==m-1 && obstacleGrid[i][j]!=1){
+            return 1;
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int d=0,r=0;
+        d=helper(obstacleGrid,n,m,i+1,j);
+        r=helper(obstacleGrid,n,m,i,j+1);
+        
+        return dp[i][j]=r+d;
+    }
+};
+
+
+//Method-3 (dp-tabulation) (using same matrix)
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obs_grid) {
@@ -40,7 +100,7 @@ public:
 };
 
 
-//Method-2 (dp-tabulation) (using separate matrix)
+//Method-4 (dp-tabulation) (using separate matrix)
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obs_grid) {
