1693b66e412c3c486b2d6b4ded3150c51a03ef40
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-02 00:55:07 +0530

    198. House Robber

--- LEETCODE/LEETCODE-CPP/198.cpp
+++ LEETCODE/LEETCODE-CPP/198.cpp
@@ -74,3 +74,25 @@ public:
         return dp[n]=max(sum1,sum2);
     }
 };
+
+class Solution {
+public:
+    int dp[101];
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+
+        if(n==1){
+            return nums[0];
+        }
+
+        memset(dp, 0, sizeof(dp));
+        
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+
+        return dp[n-1];
+    }
+};
