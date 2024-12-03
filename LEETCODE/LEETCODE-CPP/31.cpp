45b8dde793806a6078cdef0ab1f55a1863a78820
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-03 14:26:33 +0530

    31. Next Permutation

--- LEETCODE/LEETCODE-CPP/31.cpp
+++ LEETCODE/LEETCODE-CPP/31.cpp
@@ -1,28 +1,38 @@
 //31. Next Permutation
 //https://leetcode.com/problems/next-permutation/
 
-//Method-1
+//Method-1 (2-pointers)
 class Solution {
-    public:
+public:
     void nextPermutation(vector<int>& nums) {
-     int n=nums.size();
-     int l,k;
-        for( k=n-2;k>=0;k--)
-            if(nums[k]<nums[k+1])
-                break;
+        int k,l;
+        int n=nums.size();
         
-        if(k<0)
-            reverse(nums.begin(),nums.end());
-        else
-        {
-            for(l=n-1;l>k;l--)
-                if(nums[l]>nums[k])
-                    break;
-            
-            swap(nums[l],nums[k]);
-            reverse(nums.begin()+k+1,nums.end());
+        //finding the pivot i.e. trending from where the increasing trend decreases
+        for(k=n-2;k>=0;k--){
+            if(nums[k]<nums[k+1]){
+                break;
+            }
         }
         
+        //if no pivot (them the existing i/p has no next permutation)
+        if(k<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            //finding the element that is just greater than the nums[k]
+            for(l=n-1;l>k;l--){
+                if(nums[l]>nums[k]){
+                    break;
+                }
+            }
+            
+            //swaping both elements
+            swap(nums[k], nums[l]);
+            
+            //the resultant o/p may not be the next permutation so fixing it
+            reverse(nums.begin()+k+1, nums.end());
+        }
     }
 };
 
