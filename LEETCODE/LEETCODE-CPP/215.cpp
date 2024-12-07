ef2f60289706fd9cbb190ebcfe4656279d1b92fb
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-08 00:18:29 +0530

    215. Kth Largest Element in an Array

--- LEETCODE/LEETCODE-CPP/215.cpp
+++ LEETCODE/LEETCODE-CPP/215.cpp
@@ -1,6 +1,7 @@
 //215. Kth Largest Element in an Array
+//https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-//Method 1
+//Method 1 sorting in descending order
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -10,7 +11,7 @@ public:
     }
 };
 
-//Method 2
+//Method-2 using minheap
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
@@ -29,5 +30,24 @@ public:
         }        
         
         return q.top();
+    }
+};
+
+//Method-3 using minheap (clear code)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<int, vector<int>,greater<int>>pq;
+        
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+            
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
     }
 };
