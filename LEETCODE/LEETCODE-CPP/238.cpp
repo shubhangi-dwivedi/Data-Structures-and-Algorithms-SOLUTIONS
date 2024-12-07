579d2b48b6e7b877a7e4f102d4291bc1a7bc48e1
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-08 00:25:41 +0530

    238. Product of Array Except Self

--- LEETCODE/LEETCODE-CPP/238.cpp
+++ LEETCODE/LEETCODE-CPP/238.cpp
@@ -32,3 +32,25 @@ public:
         return nums;
     }
 };
+
+//Method-2 using prefix sum(int this case product)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(n),prefix(n+1),suffix(n+1);
+        prefix[0]=1;
+        suffix[n-1]=1;
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1]*nums[i-1];
+            suffix[n-1-i] = suffix[n-i]*nums[n-i];
+        }
+        
+        for(int i=0;i<n;i++){
+            ans[i] = prefix[i]*suffix[i];
+        }
+        
+        return ans;
+        
+    }
+};
