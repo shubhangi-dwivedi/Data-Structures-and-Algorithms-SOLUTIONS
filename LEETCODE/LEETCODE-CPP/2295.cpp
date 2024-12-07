db7390314493052871de57fcb6a8d8210a984b15
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-07 23:53:04 +0530

    2295. Replace Elements in an Array

---
+++ LEETCODE/LEETCODE-CPP/2295.cpp
@@ -0,0 +1,28 @@
+//2295. Replace Elements in an Array
+//https://leetcode.com/problems/replace-elements-in-an-array/
+
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        unordered_map<int,int>mp;
+        int m=nums.size(), n=operations.size();
+        
+        for(int i=0;i<m;i++){
+            mp[nums[i]]=i;
+        }
+        
+        for(int i=0;i<n;i++){
+            mp[operations[i][1]]=mp[operations[i][0]];
+            
+            mp[operations[i][0]]=-1;
+        }
+        
+        for(auto i: mp){
+            if(i.second>=0){
+                nums[i.second]=i.first;
+            }
+        }
+        
+        return nums;
+    }
+};
