e7fcd67b4f473f6b66e839d820aec9c16099ecd6
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-07 23:58:35 +0530

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

--- LEETCODE/LEETCODE-CPP/1455.cpp
+++ LEETCODE/LEETCODE-CPP/1455.cpp
@@ -1,6 +1,7 @@
 //1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
 //https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
 
+//Method -1 using two-pointer
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
