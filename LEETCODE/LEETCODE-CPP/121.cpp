0d4c119bb5aaf9fa46e81c39efd8a071e54ca8de
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-08 00:27:35 +0530

    121. Best Time to Buy and Sell Stock

--- LEETCODE/LEETCODE-CPP/121.cpp
+++ LEETCODE/LEETCODE-CPP/121.cpp
@@ -1,7 +1,37 @@
 //121. Best Time to Buy and Sell Stock
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-//Method-1
+//Method-1 using recursion
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int profit=0;
+        
+        if(n==1){
+            return profit;
+        }
+        
+        helper(prices,0,1,n,profit);
+            
+        return profit;
+    }
+    
+    void helper(vector<int>& prices, int i, int j, int n, int &profit){
+        if(i>=n || j>=n){
+            return;
+        }
+        
+        if(j>i){
+            profit=max(profit, prices[j]-prices[i]);
+            
+            helper(prices, i+1,j,n, profit);
+            helper(prices, i, j+1,n, profit);
+        }
+    }
+};
+
+//Method-2
 class Solution {
 public:
     int maxProfit(vector<int>& prices) 
