dd0fbbb35ab5573ecf227c5b05bb2ee9254d643a
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-08 00:01:09 +0530

    2295. Replace Elements in an Array

--- LEETCODE/LEETCODE-CPP/2295.cpp
+++ LEETCODE/LEETCODE-CPP/2295.cpp
@@ -1,6 +1,7 @@
 //2295. Replace Elements in an Array
 //https://leetcode.com/problems/replace-elements-in-an-array/
 
+//Method-1 using hashmap
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
