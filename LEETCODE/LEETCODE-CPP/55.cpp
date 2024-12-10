f3bbb454faf2c09015193dba6dbec2f5e0aa5c50
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-10 21:57:22 +0530

    55. Jump Game

--- LEETCODE/LEETCODE-CPP/55.cpp
+++ LEETCODE/LEETCODE-CPP/55.cpp
@@ -6,13 +6,20 @@ public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
         
-        int end=n-1;
-        for(int i=n-2;i>=0;i--) //similar to minimum jump to reach the end
+        int end=n-1; // The goal is to reach the last index, so set 'end' as the last index
+
+        // Loop from the second last element to the first
+        for(int i=n-2;i>=0;i--) //similar to minimum jumps to reach the end
+
+            // If it's possible to reach or surpass the 'end' position from index 'i'
             if(nums[i]+i>=end)
-                end=i;
+                end=i; // Update 'end' to this position, meaning we can now reach or jump from here
         
+        // If we managed to reach the first index (end == 0), return true
         if(end==0)
             return true;
+
+        // Otherwise, return false
         return false;
     }
 };
