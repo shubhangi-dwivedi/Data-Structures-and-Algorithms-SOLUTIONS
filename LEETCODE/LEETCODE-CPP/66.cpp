154e2aefd821c0a997ddca626ef3aece9efcc74e
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2024-12-11 00:23:50 +0530

    66. Plus One

--- LEETCODE/LEETCODE-CPP/66.cpp
+++ LEETCODE/LEETCODE-CPP/66.cpp
@@ -1,39 +1,26 @@
 //66. Plus One
-
+//https://leetcode.com/problems/plus-one/description/
 
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
         int n=digits.size();
-        int sum=0,rem=0;
         
-        if(n==1 && digits[0]<=8)
-        {
-            digits[0]+=1;
-            return digits;
-        }
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            if(digits[i]!=9)
-            {
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]!=9){
                 digits[i]++;
                 break;
             }
             
-            
-            if(digits[i]==9 && i!=0)
+            if(digits[i]==9 && i!=0){
                 digits[i]=0;
-
-            else
-            {
+            }
+            else{
                 digits[i]=0;
                 digits.insert(digits.begin(),1);
             }
-        }  
+        }
         
         return digits;
-        
     }
 };
