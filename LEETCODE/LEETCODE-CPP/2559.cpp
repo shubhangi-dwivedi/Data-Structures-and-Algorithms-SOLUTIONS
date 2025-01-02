b5e90d57919f469016eaf82082f4e832cdc1cd94
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-01-02 23:51:40 +0530

    2559. Count Vowel Strings in Ranges

--- LEETCODE/LEETCODE-CPP/2559.cpp
+++ LEETCODE/LEETCODE-CPP/2559.cpp
@@ -1,6 +1,7 @@
 //2559. Count Vowel Strings in Ranges
 //https://leetcode.com/problems/count-vowel-strings-in-ranges
 
+//Method-1 Prefix sum
 class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
