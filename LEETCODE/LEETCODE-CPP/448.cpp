74cc8619679bf4acb2ae699255425df80609705f
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-01-05 17:04:53 +0530

    448. Find All Numbers Disappeared in an Array

--- LEETCODE/LEETCODE-CPP/448.cpp
+++ LEETCODE/LEETCODE-CPP/448.cpp
@@ -27,3 +27,48 @@ public:
 };
 
 //[1,2,2,3,3,4,7,8]
+
+
+//Methos-2 using hashmap
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n=nums.size();
+
+        unordered_map<int,int> freq;
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(freq.find(i+1)==freq.end()){
+                ans.push_back(i+1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+//Method-3 using set
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n=nums.size();
+
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(st.find(i+1)==st.end()){
+                ans.push_back(i+1);
+            }
+        }
+
+        return ans;
+    }
+};
