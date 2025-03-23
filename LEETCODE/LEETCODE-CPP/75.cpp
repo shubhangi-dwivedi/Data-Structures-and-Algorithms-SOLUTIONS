9fa152db0e258add23c02d87414d64d4c1329ed5
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-03-23 23:43:22 +0530

    75. Sort Colors

--- LEETCODE/LEETCODE-CPP/75.cpp
+++ LEETCODE/LEETCODE-CPP/75.cpp
@@ -1,41 +1,123 @@
-//75. Sort Colors
+// 75. Sort Colors
+// https://leetcode.com/problems/sort-colors/
 
-class Solution {
+// Method-1 (brute force)
+class Solution
+{
 public:
-    void sortColors(vector<int>& nums) {
-        int count0=0,count1=0,count2=0;
-        
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++){
-            switch(nums[i]){
-                case 0: count0++;
-                        break;
-                case 1: count1++;
-                        break;
-                case 2: count2++;
-                    break;
+    void sortColors(vector<int> &nums)
+    {
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            switch (nums[i])
+            {
+            case 0:
+                count0++;
+                break;
+            case 1:
+                count1++;
+                break;
+            case 2:
+                count2++;
+                break;
             }
         }
-        
-        int i=0;
-        while(count0){
-            nums[i]=0;
+
+        int i = 0;
+        while (count0)
+        {
+            nums[i] = 0;
             count0--;
             i++;
         }
-        
-        while(count1){
-            nums[i]=1;
+
+        while (count1)
+        {
+            nums[i] = 1;
             count1--;
             i++;
         }
-        
-        while(count2){
-            nums[i]=2;
+
+        while (count2)
+        {
+            nums[i] = 2;
             count2--;
             i++;
         }
-        
+    }
+};
+
+// Method-2 (brute force, just another way to write it)
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int zeros = 0, ones = 0, twos = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 0)
+            {
+                zeros++;
+            }
+            else if (nums[i] == 1)
+            {
+                ones++;
+            }
+            else
+            {
+                twos++;
+            }
+        }
+
+        for (int i = 0; i < zeros; i++)
+        {
+            nums[i] = 0;
+        }
+
+        for (int i = zeros; i < zeros + ones; i++)
+        {
+            nums[i] = 1;
+        }
+
+        for (int i = zeros + ones; i < n; i++)
+        {
+            nums[i] = 2;
+        }
+    }
+};
+
+// Method-3 (3-pointers or Dutch National Flag Algo)
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                low++, mid++;
+            }
+            else if (nums[mid] == 2)
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+            else
+            {
+                mid++;
+            }
+        }
     }
 };
