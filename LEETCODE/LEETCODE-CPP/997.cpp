141a770a8a0d71a764be386f8bc0787867ab1b14
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-04-07 00:47:36 +0530

    997. Find the Town Judge

--- LEETCODE/LEETCODE-CPP/997.cpp
+++ LEETCODE/LEETCODE-CPP/997.cpp
@@ -1,24 +1,26 @@
-//997. Find the Town Judge
+// 997. Find the Town Judge
+// https://leetcode.com/problems/find-the-town-judge/
 
-class Solution {
+class Solution
+{
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        
-        int n2=trust.size();
-        
-        vector<pair<int,int>> res(n+1,{0,0});
-        
-        for(int i=0;i<n2;i++)
+    int findJudge(int n, vector<vector<int>> &trust)
+    {
+
+        int n2 = trust.size();
+
+        vector<pair<int, int>> res(n + 1, {0, 0});
+
+        for (int i = 0; i < n2; i++)
         {
-            res[trust[i][0]].first+=1;
-            res[trust[i][1]].second+=1;
+            res[trust[i][0]].first += 1;
+            res[trust[i][1]].second += 1;
         }
-        
-        for(int i=1;i<n+1;i++)
-            if(res[i].second==n-1 && res[i].first==0)
+
+        for (int i = 1; i < n + 1; i++)
+            if (res[i].second == n - 1 && res[i].first == 0)
                 return i;
-        
+
         return -1;
-        
     }
 };
