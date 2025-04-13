ccd2edf8b28e0af4e1dbb92e117900157c79a82f
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-04-14 00:35:13 +0530

    2351. First Letter to Appear Twice

--- LEETCODE/LEETCODE-CPP/2351.cpp
+++ LEETCODE/LEETCODE-CPP/2351.cpp
@@ -1,19 +1,46 @@
-//2351. First Letter to Appear Twice
-//https://leetcode.com/problems/first-letter-to-appear-twice/
+// 2351. First Letter to Appear Twice
+// https://leetcode.com/problems/first-letter-to-appear-twice/
 
-class Solution {
+// Method-1 using set
+class Solution
+{
 public:
-    char repeatedCharacter(string s) {
-        int n=s.length(),i;
+    char repeatedCharacter(string s)
+    {
+        int n = s.length(), i;
         set<char> st;
 
-        for(i=0;i<n;i++){
-            if(st.find(s[i])!=st.end())
+        for (i = 0; i < n; i++)
+        {
+            if (st.find(s[i]) != st.end())
                 break;
-            
+
             st.insert(s[i]);
         }
 
         return s[i];
+    }
+};
+
+// Method-2 using hashmap
+class Solution
+{
+public:
+    char repeatedCharacter(string s)
+    {
+        int n = s.length();
+        unordered_map<char, int> m;
+
+        for (int i = 0; i < n; i++)
+        {
+            m[s[i]]++;
+
+            if (m[s[i]] == 2)
+            {
+                return s[i];
+            }
+        }
+
+        return -1;
     }
 };
