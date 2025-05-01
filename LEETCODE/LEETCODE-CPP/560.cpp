4021c9a6907c8ac62dd78e81ab5a78189f91db52
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-05-01 16:07:09 +0530

    560. Subarray Sum Equals K

--- LEETCODE/LEETCODE-CPP/560.cpp
+++ LEETCODE/LEETCODE-CPP/560.cpp
@@ -1,51 +1,56 @@
-//560. Subarray Sum Equals K
-//https://leetcode.com/problems/subarray-sum-equals-k/
+// 560. Subarray Sum Equals K
+// https://leetcode.com/problems/subarray-sum-equals-k/
 
-//Method-1 (Time- O(n^2)) (TLE)
-class Solution {
+// Method-1 (Time- O(n^2)) (TLE)
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int sum=0, count=0;
-        
-        for(int i=0;i<n;i++){
-            sum=0;
-            
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        int sum = 0, count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum = 0;
+
+            for (int j = i; j < n; j++)
+            {
+                sum += nums[j];
+                if (sum == k)
+                {
                     count++;
                 }
             }
         }
-        
+
         return count;
     }
 };
 
-
-//Method-2 Prefix Sum (Time- O(n)) 
-class Solution {
+// Method-2 Prefix Sum (Time- O(n))
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int prefSum=0, res=0;
-        unordered_map<int,int>freq;
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> sumFreq;
+        int res = 0, prefSum = 0;
 
-        freq[0]=1;
+        // means sum freq equals 0 occurred 1 time
+        sumFreq[0] = 1;
+        for (auto i : nums)
+        {
+            prefSum += i;
 
-        for(auto i:nums){
-            //calculation prefix sum
-            prefSum+=i; 
+            if (sumFreq.find(prefSum - k) != sumFreq.end())
+            {
+                res += sumFreq[prefSum - k];
+            }
 
-            //calculating subarrays
-            //res=res + freq[prefSum-k]
-            if(freq.find(prefSum-k)!=freq.end())
-                res+=freq[prefSum-k];
-
-            freq[prefSum]++;
+            sumFreq[prefSum]++;
         }
 
-        //counts of subarrays
         return res;
     }
 };
