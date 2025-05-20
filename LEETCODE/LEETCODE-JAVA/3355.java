65bf59b65bf682919349efa3a3b2633effa524eb
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-05-20 08:59:09 +0530

    3355. Zero Array Transformation I

--- LEETCODE/LEETCODE-JAVA/3355.java
+++ LEETCODE/LEETCODE-JAVA/3355.java
@@ -5,11 +5,7 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n1=nums.length, n2=queries.length;
-
         int[] diff=new int[n1+1];
-        for(int i=0;i<n1+1;i++){
-            diff[i]=0;
-        }
 
         for(int i=0;i<n2;i++){
             diff[queries[i][0]]+=1;
