342742c5ab2d148b181855e833b8fcc32741eba8
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-08-19 23:08:56 +0530

    200. Number of Islands

--- LEETCODE/LEETCODE-PYTHON/200.py
+++ LEETCODE/LEETCODE-PYTHON/200.py
@@ -26,3 +26,34 @@ class Solution:
                     dfs(i,j)
 
         return count
+    
+# Method -2 using bfs
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m,n =len(grid), len(grid[0])
+        vis=defaultdict(bool)
+
+        dir= [(-1,0),(1,0),(0,-1),(0,1)]
+        
+        def bfs(i,j):
+            q= deque()
+            q.append((i,j))
+            grid[i][j] = '0'  # mark visited by sinking the island
+
+            while(q):
+                x,y=q.popleft()
+                for dx, dy in dir:
+                    tempx, tempy= x+dx, y+dy
+                    if 0<=tempx<m and 0<=tempy<n and grid[tempx][tempy]=='1':
+                        grid[tempx][tempy]='0'
+                        q.append((tempx, tempy))
+
+
+        count=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    bfs(i, j)
+                    count += 1
+
+        return count
