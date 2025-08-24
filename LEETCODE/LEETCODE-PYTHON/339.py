417d7ac3ae6361505ed6149bacbb77e83b827b00
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-08-24 11:48:12 +0530

    339. Nested List Weight Sum

---
+++ LEETCODE/LEETCODE-PYTHON/339.py
@@ -0,0 +1,62 @@
+# 339. Nested List Weight Sum
+# https://leetcode.com/problems/nested-list-weight-sum/description/
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+# Methos-1 using dfs
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        
+        def dfs(nested_list, depth):
+            sum=0
+
+            for ele in nested_list:
+                if ele.isInteger():
+                    sum+=ele.getInteger()*depth
+                else:
+                    sum+=dfs(ele.getList(), depth+1)
+
+            return sum
+
+        return dfs(nestedList, 1)
